431ceee3e29d802f84924982cf38e9ad08b072ad
kanmadozo <trakyrichardkanm3@gmail.com>
2020-03-11 16:06:24 +0100

    Final

---
+++ js/app.js
@@ -0,0 +1,51 @@
+const courses = document.querySelector('#courses-list'),
+ shoppingCartContent = document.querySelector('#cart-content tbody');
+
+ loadEventListeners();
+
+function loadEventListeners(){
+    //when new course is added
+    courses.addEventListener('click', buyCourse);
+
+function buyCourse(e){
+    if(e.target.classList.contains('add-to-cart')){
+        //read the course value
+     const course = e.target.parentElement.parentElement;
+        getCourseInfo(course);
+    }
+}};
+
+function getCourseInfo(course) {
+    //create an Object with course data
+    const courseInfo = {
+        image: course.querySelector('img').src,
+        title: course.querySelector('h4').textContent,
+        price: course.querySelector('.price span').textContent,
+        id: course.querySelector('a').getAttribute('data-id')
+    }
+    addToCart(courseInfo);
+}
+
+function addToCart(course){
+    const row = document.createElement('tr');
+
+    row.innerHTML = `
+<tr>
+    <td>
+        <img src="${course.image}" width="100">    
+    </td>
+    <td>
+        ${course.title}
+    </td>
+     <td>
+        ${course.price}
+    </td>
+     <td>
+        <a href="#" class="remove" data-id="${course.id}">X</a>
+    </td>
+</tr>
+`
+    ;
+
+    shoppingCartContent.appendChild(row);
+}
